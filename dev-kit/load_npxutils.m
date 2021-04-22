06703b05dcec28cbbcf4afaa67140ca70e45cae1
Andrew Janke <andrew@apjanke.net>
2021-04-22 03:35:31 -0400

    Fix stringification in library loading

--- dev-kit/load_npxutils.m
+++ dev-kit/load_npxutils.m
@@ -2,7 +2,7 @@ function load_npxutils
 % Load the npxutils library
 
 repoDir = npxutils_repo_root;
-mcodeDir = [repoDir '/Mcode'];
+mcodeDir = repoDir + "/Mcode";
 addpath(mcodeDir);
 
 fprintf('Loaded Neuropixel Utils %s from %s\n', ...
